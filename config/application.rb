a173468e9f75d3397eb008aef59012f6f871b560
mythlabs <amitbisht744@gmail.com>
2019-04-14 12:28:34 +0530

    Added api throttling checks with rack attack

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module IdealRailsApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.middleware.use Rack::Attack
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
@@ -31,7 +31,7 @@ module IdealRailsApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-
+    
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
